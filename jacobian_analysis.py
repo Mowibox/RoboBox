636e6d72c1c36c13d641885e1045aebaf0c818c8
Mowibox <ousmane.thiongane@ensea.fr>
2025-01-12 16:45:07 +0100

    Improving jacobian_analysis

--- jacobian_analysis.py
+++ jacobian_analysis.py
@@ -47,12 +47,18 @@ def jacobian_analysis(J: sp.Matrix, variables: list[sp.Symbol]):
     find_null(J)
 
     print("\n==== Complementary nullspace of the Jacobian ====")
-    nullspace = J.T.nullspace()
-    if not nullspace:
+    complementary_nullspace = J.T.nullspace()
+    if not complementary_nullspace:
         print("The complementary nullspace is only the 0 vector")
     else:
-        for i, vec in enumerate(nullspace, start=1):
+        for i, vec in enumerate(complementary_nullspace, start=1):
             simplified_vec = sp.simplify(vec)
             print(f"Basis vector n°{i}:")
             sp.pprint(simplified_vec)
+
+    print("\n==== Dimension checking ====")
+    print("dim Range(J) + dim Null(J^T):", len(J.columnspace()) + len(complementary_nullspace))
+    print("..should equal the task-space dimension (rows of the Jacobian).")
+    print("dim Range(J^T) + dim Null(J):", len(J.T.columnspace()) + len(J.nullspace()))
+    print("..should equal the joint-space dimension (columns of the Jacobian).")
 
